7db7038ddc466271992c5f2d91409935fbd32584
Andre Mroz <mathmonkey4@gmail.com>
2014-05-27 15:51:10 -0400

    asda

--- client/home.js
+++ client/home.js
@@ -4,12 +4,16 @@ $(document).ready(function(){
    $('#video').toggleClass('hidden');
   });
 
-  $('#background').mouseenter(function(){
-  	$(this).animate({'opacity':.7},200);
-    $(this).text("Watch the Video!");
-	});
-  $('#background').mouseleave(function(){
-  	$(this).animate({'opacity':1},200);
-    $(this).text("");
-  });
+  //$('#background').mouseenter(function(){
+  //	$(this).animate({'opacity':.7},200);
+  //  $(this).text("Watch the Video!");
+	//});
+  //$('#background').mouseleave(function(){
+  //	$(this).animate({'opacity':1},200);
+  //  $(this).text("");
+  //});
+
+  $(window).scroll(function(){
+  })
+
 });
